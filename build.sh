91858fc894fd077c031b2840db24eea9ce7233e3
Andrey Vasiliev <13902167+roninest@users.noreply.github.com>
2020-06-02 17:19:36 +0300

    No space left on device: trying use less space

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 docker build . -t balrogcpp/context-project-demo
 docker create -ti --name abc context bash
 mkdir artifacts
-docker cp abc:/mnt/context-demo/artifacts/refman.pdf artifacts
 docker cp abc:/mnt/context-demo/install/ContextProjectDemo-Release-`git rev-parse -q HEAD`.zip artifacts
-#docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Linux-x64.tar.gz artifacts
-#docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Linux-x64.7z artifacts
-#docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Windows-x64.zip artifacts
-#docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Windows-x64.7z artifacts
-#docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Windows-x64.exe artifacts
+# docker cp abc:/mnt/context-demo/artifacts/refman.pdf artifacts
+# docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Linux-x64.tar.gz artifacts
+# docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Linux-x64.7z artifacts
+# docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Windows-x64.zip artifacts
+# docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Windows-x64.7z artifacts
+# docker cp abc:/mnt/context-demo/artifacts/ContextProjectDemo-Release-Windows-x64.exe artifacts
 docker rm -f abc
 
 exit 0
