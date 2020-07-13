a0dfbc2645354db6660faf4e04e9a078885a6d61
Andrey Vasiliev <foursilico@gmail.com>
2020-07-13 15:16:43 +0300

    200 MB Docker image size optimization

--- push-dockers.sh
+++ push-dockers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker login &&\
 docker build . -f gcc/Dockerfile -t balrogcpp/mingw-gcc10-cmake &&\
 docker build . -f thirdparty/Dockerfile -t balrogcpp/context-project-dependencies &&\
+docker login &&\
 docker push balrogcpp/mingw-gcc10-cmake &&\
 docker push balrogcpp/context-project-dependencies
 
