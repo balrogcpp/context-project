d26461aa1aa7ab84e0b7f21fa250720c30c4921b
Andrey Vasiliev <foursilico@gmail.com>
2020-08-03 19:50:51 +0300

    GCC 10.2.0

--- push-dockers.sh
+++ push-dockers.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build . -f gcc/Dockerfile -t balrogcpp/mingw-gcc10-cmake &&\
+docker build . -f gcc/Dockerfile -t balrogcpp/gcc-mingw-x86_64 &&\
 docker build . -f thirdparty/Dockerfile -t balrogcpp/context-project-dependencies &&\
 docker login &&\
-docker push balrogcpp/mingw-gcc10-cmake &&\
+docker push balrogcpp/gcc-mingw-x86_64 &&\
 docker push balrogcpp/context-project-dependencies
 
 exit 0
