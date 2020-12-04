05175364c68b961a65fb39f135eb61b5e2878676
Andrey Vasiliev <foursilico@gmail.com>
2020-12-05 01:13:59 +0200

    CMake: clean-up

--- CMake/toolchain-mingw.cmake
+++ CMake/toolchain-mingw.cmake
@@ -30,7 +30,6 @@ set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
 set(CMAKE_ASM-ATT_COMPILER ${TOOLCHAIN_PREFIX}-as)
-set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # target environment on the build host system
