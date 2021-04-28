619f4e28bb429fe6a4c960e1b7619e0145a7aba8
Andrew Vasiliev <foursilico@gmail.com>
2021-04-29 02:40:51 +0300

    dockerfiles optimization

--- build.sh
+++ build.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-docker build --build-arg GIT_HASH=`git rev-parse --short=8 HEAD`  . -f Docker/xio-linux/Dockerfile -t balrogcpp/xio-linux
-docker build --build-arg GIT_HASH=`git rev-parse --short=8 HEAD`  . -f Docker/xio-cross/Dockerfile -t balrogcpp/xio-cross
-docker create -ti --name abc balrogcpp/xio-linux bash
 mkdir artifacts
-docker cp abc:/mnt/build/artifacts/ContextProjectDemo-linux-clang-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts
+
+
+docker build --build-arg GIT_HASH=`git rev-parse --short=8 HEAD`  . -f docker/xio-linux/Dockerfile -t balrogcpp/xio-linux \
+&& docker create -ti --name abc balrogcpp/xio-linux bash \
+&& docker cp abc:/mnt/build/artifacts/ContextProjectDemo-linux-clang-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts
+
 docker rm -f abc
-docker create -ti --name abc balrogcpp/xio-cross bash
-docker cp abc:/mnt/build/artifacts/ContextProjectDemo-windows-clang-mingw-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts
-docker cp abc:/mnt/build/artifacts/ContextProjectDemo-apple-darwin-clang-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts
+
+
+
+docker build --build-arg GIT_HASH=`git rev-parse --short=8 HEAD`  . -f docker/xio-cross/Dockerfile -t balrogcpp/xio-cross \
+&& docker create -ti --name abc balrogcpp/xio-cross bash \
+&& docker cp abc:/mnt/build/artifacts/ContextProjectDemo-windows-clang-mingw-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts \
+&& docker cp abc:/mnt/build/artifacts/ContextProjectDemo-apple-darwin-clang-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts
+
 docker rm -f abc
+
+
+
+docker build --build-arg GIT_HASH=`git rev-parse --short=8 HEAD`  . -f docker/xio-android/Dockerfile -t balrogcpp/xio-android \
+&& docker create -ti --name abc balrogcpp/xio-android bash \
+&& docker cp abc:/mnt/build/artifacts/app-arm64-v8a-release.apk artifacts \
+&& docker cp abc:/mnt/build/artifacts/app-x86_64-release.apk  artifacts
+
+docker rm -f abc
+
 
 exit 0
