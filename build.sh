c9870c66a4e7455193bde82e680f5fac5df6c78d
Andrew Vasiliev <foursilico@gmail.com>
2021-05-10 01:33:37 +0300

    Fixes

--- build.sh
+++ build.sh
@@ -14,7 +14,8 @@ docker rm -f abc
 docker build --build-arg GIT_HASH=`git rev-parse --short=8 HEAD`  . -f docker/xio-cross/Dockerfile -t balrogcpp/xio-cross \
 && docker create -ti --name abc balrogcpp/xio-cross bash \
 && docker cp abc:/mnt/build/artifacts/ContextProjectDemo-windows-clang-mingw-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts \
-&& docker cp abc:/mnt/build/artifacts/ContextProjectDemo-apple-darwin-clang-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts
+&& docker cp abc:/mnt/build/artifacts/ContextProjectDemo-apple-darwin-clang-x86_64-`git rev-parse --short=8 HEAD`.zip artifacts \
+&& docker cp abc:/mnt/build/artifacts/app-arm64-v8a-`git rev-parse --short=8 HEAD`.apk artifacts
 
 docker rm -f abc
 
