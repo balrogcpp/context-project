4c558dd1872007dce99e2bd61c7d45a16d45c864
Andrew Vasiliev <foursilico@gmail.com>
2021-05-10 00:40:23 +0300

    Adjust gitlab-ci

--- push-dockers.sh
+++ push-dockers.sh
@@ -3,9 +3,13 @@
 
 docker build . -f docker/clang-linux/Dockerfile -t balrogcpp/clang-linux \
 && docker build . -f docker/clang-cross/Dockerfile -t balrogcpp/clang-cross \
+&& docker build . -f docker/deps-cross/Dockerfile -t balrogcpp/deps-cross \
+&& docker build . -f docker/deps-linux/Dockerfile -t balrogcpp/deps-linux \
 && docker login \
 && docker push balrogcpp/clang-linux \
 && docker push balrogcpp/clang-cross \
+&& docker push balrogcpp/deps-linux \
+&& docker push balrogcpp/deps-cross \
 
 
 exit 0
