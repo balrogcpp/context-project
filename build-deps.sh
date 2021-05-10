d30c94684553e98c3853562c2fb4935909974b80
Andrew Vasiliev <foursilico@gmail.com>
2021-05-11 02:09:32 +0300

    Fixes

--- build-deps.sh
+++ build-deps.sh
@@ -3,8 +3,8 @@
 
 docker build . -f docker/clang-linux/Dockerfile -t balrogcpp/clang-linux \
 && docker build . -f docker/clang-cross/Dockerfile -t balrogcpp/clang-cross \
-&& docker build . -f docker/deps-cross/Dockerfile -t balrogcpp/deps-cross \
 && docker build . -f docker/deps-linux/Dockerfile -t balrogcpp/deps-linux \
+&& docker build . -f docker/deps-cross/Dockerfile -t balrogcpp/deps-cross \
 && docker login \
 && docker push balrogcpp/clang-linux \
 && docker push balrogcpp/clang-cross \
