25bed3251de33edf03f60fd311be77f74ec496f6
Andrew Vasiliev <foursilico@gmail.com>
2022-01-26 03:21:57 +0200

    Custom imgui code

---
+++ Source/Glue/include/GUI/ToggleButton.h
@@ -0,0 +1,12 @@
+// This source file is part of "glue project". Created by Andrey Vasiliev
+#pragma once
+
+namespace ImGui {
+
+// Taken from https://github.com/ocornut/imgui/issues/1537
+void ToggleButton(const char* str_id, bool* v);
+
+// Taken from https://github.com/ocornut/imgui/issues/4722
+bool ColoredButtonV1(const char* label, const ImVec2& size_arg, ImU32 text_color, ImU32 bg_color_1, ImU32 bg_color_2);
+
+}  // namespace ImGui
