b111dd9e823b506f9533a3114cb61023563d6e35
Andrey Vasiliev <foursilico@gmail.com>
2022-04-14 13:14:59 +0300

    Fog modes

--- Programs/Core/PBR/fog.glsl
+++ Programs/Core/PBR/fog.glsl
@@ -4,32 +4,34 @@
 #define FOG_GLSL
 
 //----------------------------------------------------------------------------------------------------------------------
-float EXP (float x)
+vec3 ApplyFogLinear(const vec3 color, const vec4 fog_params, const vec3 fog_color, const float depth)
 {
-    //return 1.0 + x + 0.5*x*x;
-    return exp(x);
+    float fog_factor = clamp((fog_params.z - depth) * fog_params.w, 0.0, 1.0);
+    return mix(color, fog_color, fog_factor);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-vec3 EXP (vec3 x)
+vec3 ApplyFogExp(const vec3 color, const vec4 fog_params, const vec3 fog_color, const float depth)
 {
-    //return 1.0 + x + vec3(0.5)*x*x;
-    return exp(x);
+    float x = depth * fog_params.x;
+    float fog_factor = 1.0 - clamp(1.0 / exp(x), 0.0, 1.0);
+    return mix(color, fog_color, fog_factor);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-vec3 ApplyFog(vec3 color, vec4 fog_params, vec3 fog_color, float depth)
+vec3 ApplyFogExp2(const vec3 color, const vec4 fog_params, const vec3 fog_color, const float depth)
 {
-    float fog_value = 1.0 - clamp(1.0/EXP(depth * fog_params.x), 0.0, 1.0);
-    return mix(color, fog_color, fog_value);
+    float x = depth * fog_params.x;
+    float fog_factor = 1.0 - clamp(1.0 / exp(x * x), 0.0, 1.0);
+    return mix(color, fog_color, fog_factor);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-vec4 ApplyFog(vec4 color, vec4 fog_params, vec3 fog_color, float depth)
+vec3 ApplyFog(const vec3 color, const vec4 fog_params, const vec3 fog_color, const float depth)
 {
-    float fog_value = 1.0 - clamp(1.0/EXP(depth * fog_params.x), 0.0, 1.0);
-    color.rgb = mix(color.rgb, fog_color, fog_value);
-    return color;
+    float x = depth * fog_params.x;
+    float fog_factor = 1.0 - clamp(1.0 / exp(x), 0.0, 1.0);
+    return mix(color, fog_color, fog_factor);
 }
 
 #endif // FOG_GLSL
