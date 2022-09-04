23aaf2f48039a7e0cfefa7ad6560f541db4a7fc5
Andrew Vasiliev <foursilico@gmail.com>
2022-09-04 11:37:51 +0300

    apk fetch fix

--- docker.sh
+++ docker.sh
@@ -9,5 +9,5 @@ docker cp $CONTAINER_ID:/mnt/artifacts/GlueSample_Linux_x86_64.tar.xz ./artifact
 docker cp $CONTAINER_ID:/mnt/artifacts/GlueSample_Windows_x86_64.zip ./artifacts/GlueSample_Windows_x86_64_$GIT_SHA1.zip
 docker cp $CONTAINER_ID:/mnt/artifacts/GlueSample_Darwin_x86_64.tar.xz ./artifacts/GlueSample_Darwin_x86_64_$GIT_SHA1.tar.xz
 docker cp $CONTAINER_ID:/mnt/artifacts/GlueSample_Emscripten_x86.tar.xz ./artifacts/GlueSample_Emscripten_x86_$GIT_SHA1.tar.xz
-docker cp $CONTAINER_ID:/mnt/Source/Engine/android/build/outputs/apk/release/android-arm64-v8a-release.apk ./artifacts/GlueSample_Android_aarch64_$GIT_SHA1.apk
+docker cp $CONTAINER_ID:/mnt/source/Engine/android/build/outputs/apk/release/android-arm64-v8a-release.apk ./artifacts/GlueSample_Android_aarch64_$GIT_SHA1.apk
 docker stop $CONTAINER_ID
