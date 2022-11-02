5bca9ff5dae79351f18d9af6d1930de4d7876c10
Andrew Vasiliev <foursilico@gmail.com>
2022-11-02 20:46:19 +0200

    new LogError func

--- source/Engine/Code/Assertion.h
+++ source/Engine/Code/Assertion.h
@@ -19,6 +19,20 @@ inline void LogError(const std::string &title, const std::string &text) noexcept
   logManager.logError("[" + title + "] " + text);
 }
 
+inline void LogError(int number, const std::string &description, const std::string &source, const char *type, const char *file, long line) noexcept {
+  static auto &logManager = Ogre::LogManager::getSingleton();
+
+  std::stringstream ss;
+
+  ss << type << ": " << description << " in " << source;
+
+  if (line > 0) {
+    ss << " at " << file << " (line " << line << ")";
+  }
+
+  logManager.logError(ss.str());
+}
+
 inline void LogWarning(const std::string &title, const std::string &text) noexcept {
   static auto &logManager = Ogre::LogManager::getSingleton();
   logManager.logWarning("[" + title + "] " + text);
@@ -34,9 +48,10 @@ inline void LogTrivial(const std::string &title, const std::string &text) noexce
   logManager.logMessage("[" + title + "] " + text, Ogre::LML_TRIVIAL);
 }
 
-#define LOG_ERROR(b) LogError("Assertion failed", b)
+#define LOG_ERROR(b) LogError(Ogre::Exception::ERR_RT_ASSERTION_FAILED, b, __FUNCTION__, "RuntimeAssertionException", __FILE__, __LINE__)
 #define THROW_EXCEPTION(b) \
   throw Ogre::Exception(Ogre::Exception::ERR_RT_ASSERTION_FAILED, b, __FUNCTION__, "RuntimeAssertionException", __FILE__, __LINE__)
+
 
 #ifndef NDEBUG
 #define ASSERTION(a, b) OgreAssert(a, b)
