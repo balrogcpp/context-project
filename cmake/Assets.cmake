4e42a6eaae03dd19a03cf801c9a6b2eee3654b0b
Andrew Vasiliev <foursilico@gmail.com>
2022-11-06 04:03:34 +0200

    Assets.cmake fixed when no recursion

--- cmake/Assets.cmake
+++ cmake/Assets.cmake
@@ -1,26 +1,12 @@
-macro(FlatZipSubdirectories curdir destination)
-    make_directory(${destination})
-    file(GLOB directories RELATIVE ${curdir} ${curdir}/*)
-
-    foreach (directory ${directories})
-        if (${directory} STREQUAL ".git") #omit .git directory
-            continue()
-        endif ()
-        set(filelist "")
-        if (IS_DIRECTORY ${curdir}/${directory})
-            file(GLOB_RECURSE files LIST_DIRECTORIES false ${curdir}/${directory} ${curdir}/${directory}/*)
-            make_directory(${destination}/${directory})
-            file(COPY ${files} DESTINATION ${destination}/${directory})
-            file(GLOB filelist RELATIVE ${destination}/${directory} ${destination}/${directory}/*)
-            execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${destination}/${directory} ${CMAKE_COMMAND} -E tar cf ${destination}/${directory}.bin --format=zip ${filelist})
-            file(REMOVE_RECURSE ${destination}/${directory})
-        endif ()
-    endforeach ()
-endmacro()
+# include guard
+if (_cppflags_included)
+    return()
+endif (_cppflags_included)
+set(_cppflags_included true)
 
 
 # Zip files from directory into flat zip
-macro(CopyFlatZipDirectory curdir destination)
+macro(FlatCopy curdir destination)
     make_directory(${destination})
     file(GLOB directories RELATIVE ${curdir} ${curdir}/*)
 
@@ -38,15 +24,15 @@ endmacro()
 
 
 # Zip files from directory into flat zip
-macro(ZipDirectory destination)
+macro(ZipDirectory destination extention)
     file(GLOB filelist RELATIVE ${destination} ${destination}/*)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${destination}/${directory} ${CMAKE_COMMAND} -E tar cf ${destination}.bin --format=zip ${filelist})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${destination}/${directory} ${CMAKE_COMMAND} -E tar cf ${destination}.${extention} --format=zip ${filelist})
     file(REMOVE_RECURSE ${destination})
 endmacro()
 
 
 # Zip files from directory into flat zip
-macro(FlatZipDirectory curdir destination)
+macro(FlatZipDirectory curdir destination extention)
     make_directory(${destination})
     file(GLOB directories RELATIVE ${curdir} ${curdir}/*)
 
@@ -58,23 +44,28 @@ macro(FlatZipDirectory curdir destination)
         if (IS_DIRECTORY ${curdir}/${directory})
             file(GLOB_RECURSE files LIST_DIRECTORIES false ${curdir}/${directory} ${curdir}/${directory}/*)
             file(COPY ${files} DESTINATION ${destination})
+        else ()
+            file(COPY ${curdir}/${directory} DESTINATION ${destination})
         endif ()
     endforeach ()
 
     file(GLOB filelist RELATIVE ${destination} ${destination}/*)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${destination}/${directory} ${CMAKE_COMMAND} -E tar cf ${destination}.bin --format=zip ${filelist})
+    execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${destination}/${directory} ${CMAKE_COMMAND} -E tar cf ${destination}.${extention} --format=zip ${filelist})
     file(REMOVE_RECURSE ${destination})
 endmacro()
 
 
-file(REMOVE_RECURSE ${ASSETS_DIR})
-make_directory(${ASSETS_DIR})
-CopyFlatZipDirectory(${ROOT_DIR}/source/Programs ${ASSETS_DIR}/programs)
+# do the job
+FlatCopy(${ROOT_DIR}/source/Programs ${ASSETS_DIR}/programs)
 if (ANDROID OR IOS OR EMSCRIPTEN OR GLSLES)
     file(GLOB files ${ROOT_DIR}/source/GLSLES/*)
 else ()
     file(GLOB files ${ROOT_DIR}/source/GLSL/*)
 endif ()
 file(COPY ${files} DESTINATION ${ASSETS_DIR}/programs)
-FlatZipDirectory(${ROOT_DIR}/source/Example/Assets ${ASSETS_DIR}/assets)
-ZipDirectory(${ASSETS_DIR}/programs)
+ZipDirectory(${ASSETS_DIR}/programs bin)
+
+FlatZipDirectory(${ROOT_DIR}/source/Programs ${ASSETS_DIR}/programs zip)
+FlatZipDirectory(${ROOT_DIR}/source/GLSL ${ASSETS_DIR}/glsl zip)
+FlatZipDirectory(${ROOT_DIR}/source/GLSLES ${ASSETS_DIR}/glsles zip)
+FlatZipDirectory(${ROOT_DIR}/source/Example/Assets ${ASSETS_DIR}/assets bin)
