484fe6fa4178b237e92234da203a6c7d135ac90c
Andrey Vasiliev <foursilico@gmail.com>
2022-11-13 23:06:09 +0200

    ImSpinner

--- source/Engine/imgui_user/imgui_user.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <imgui.h>
-#include "imgui_user/ImHotKey.h"
-#include "imgui_user/IconsMaterialDesign.h"
-#include "imgui_user/IconsKenney.h"
-#include "imgui_user/IconsFontAwesome5.h"
