39bd62ad31f8f748dc9be3ed1689713478b8e94d
Andrew Vasiliev <foursilico@gmail.com>
2022-12-15 20:06:05 +0200

    fix assets generation for android

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -30,6 +30,28 @@ macro(ZipDirectory destination extention)
     file(REMOVE_RECURSE ${destination})
 endmacro()
 
+# clears timestamp to 1 January 1999 00:00:00
+macro(ClearTimeStampDirectory destination)
+    if (CMAKE_HOST_UNIX)
+        execute_process(COMMAND find ${destination} -type f -exec touch -t 199901010000 {} +)
+    elseif (CMAKE_HOST_WIN32)
+        execute_process(COMMAND powershell "Get-ChildItem -force ${destination} * | Where-Object {! $_.PSIsContainer} | ForEach-Object{$_.CreationTime = (\"1 January 1999 00:00:00\")}")
+        execute_process(COMMAND powershell "Get-ChildItem -force ${destination} * | Where-Object {! $_.PSIsContainer} | ForEach-Object{$_.LastWriteTime = (\"1 January 1999 00:00:00\")}")
+        execute_process(COMMAND powershell "Get-ChildItem -force ${destination} * | Where-Object {! $_.PSIsContainer} | ForEach-Object{$_.LastAccessTime = (\"1 January 1999 00:00:00\")}")
+    endif ()
+endmacro()
+
+# clears timestamp to 1 January 1999 00:00:00
+macro(ClearTimeStamp file)
+    if (CMAKE_HOST_UNIX)
+        execute_process(COMMAND touch -t 199901010000 ${file})
+    elseif (CMAKE_HOST_WIN32)
+        execute_process(COMMAND powershell "(Get-Item ${file}).CreationTime = (\"1 January 1999 00:00:00\")")
+        execute_process(COMMAND powershell "(Get-Item ${file}).LastWriteTime = (\"1 January 1999 00:00:00\")")
+        execute_process(COMMAND powershell "(Get-Item ${file}).LastAccessTime = (\"1 January 1999 00:00:00\")")
+    endif ()
+endmacro()
+
 
 # Zip files from directory into flat zip
 macro(FlatZipDirectory curdir destination extention)
@@ -50,25 +72,9 @@ macro(FlatZipDirectory curdir destination extention)
     endforeach ()
 
     file(GLOB filelist RELATIVE ${destination} ${destination}/*)
-
-    if (UNIX)
-        execute_process(COMMAND find ${destination} -type f -exec touch -t 199901010000 {} +)
-    elseif (WIN32)
-        execute_process(COMMAND powershell "Get-ChildItem -force ${destination} * | Where-Object {! $_.PSIsContainer} | ForEach-Object{$_.CreationTime = (\"1 January 1999 00:00:00\")}")
-        execute_process(COMMAND powershell "Get-ChildItem -force ${destination} * | Where-Object {! $_.PSIsContainer} | ForEach-Object{$_.LastWriteTime = (\"1 January 1999 00:00:00\")}")
-        execute_process(COMMAND powershell "Get-ChildItem -force ${destination} * | Where-Object {! $_.PSIsContainer} | ForEach-Object{$_.LastAccessTime = (\"1 January 1999 00:00:00\")}")
-    endif ()
-
+    ClearTimeStampDirectory(${destination})
     execute_process(COMMAND ${CMAKE_COMMAND} -E chdir ${destination}/${directory} ${CMAKE_COMMAND} -E tar cf ${destination}.${extention} --format=zip ${filelist})
-
-    if (UNIX)
-        execute_process(COMMAND touch -t 199901010000 ${destination}.${extention})
-    else ()
-        execute_process(COMMAND powershell "(Get-Item ${destination}.${extention}).CreationTime = (\"1 January 1999 00:00:00\")")
-        execute_process(COMMAND powershell "(Get-Item ${destination}.${extention}).LastWriteTime = (\"1 January 1999 00:00:00\")")
-        execute_process(COMMAND powershell "(Get-Item ${destination}.${extention}).LastAccessTime = (\"1 January 1999 00:00:00\")")
-    endif ()
-
+    ClearTimeStamp(${destination}.${extention})
     file(REMOVE_RECURSE ${destination})
 endmacro()
 
