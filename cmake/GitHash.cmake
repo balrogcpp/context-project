5b1c402322fd121ad2cc55c5da27c78d81457e61
Andrew Vasiliev <foursilico@gmail.com>
2022-12-19 01:27:59 +0200

    git hash ready

--- cmake/GitHash.cmake
+++ cmake/GitHash.cmake
@@ -23,6 +23,8 @@ if (NOT GIT_SHA AND NOT GIT_SHA_SHORT AND GIT_FOUND AND IS_DIRECTORY ${CMAKE_SOU
                 OUTPUT_VARIABLE GIT_SHA
                 OUTPUT_STRIP_TRAILING_WHITESPACE
         )
+
+    # try to find out HEAD hash without main repo
     elseif (EXISTS "${CMAKE_SOURCE_DIR}/.git/HEAD")
         file(READ "${CMAKE_SOURCE_DIR}/.git/HEAD" GIT_HEAD)
         string(REGEX MATCH "[^ ]+$" GIT_HEAD ${GIT_HEAD})
