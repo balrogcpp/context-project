b10aedd2d45ad73cae524cb792346a9cc84bc808
Andrey <foursilico@gmail.com>
2022-12-18 06:18:06 +0200

    embed assets too if available

--- source/Engine/Code/EmbeddedResources.cpp
+++ source/Engine/Code/EmbeddedResources.cpp
@@ -1,21 +1,31 @@
 /// created by Andrey Vasiliev
 
+#ifdef NDEBUG
 #include <OgreRoot.h>
 #include <OgreZip.h>
-#ifdef NDEBUG
 #if __has_include("programs.zip.h")
 #include "programs.zip.h"
+#endif
+#if __has_include("assets.zip.h")
+#include "assets.zip.h"
 #endif
 #endif
 
 namespace Glue {
 void InitEmbeddedResources() {
-  auto &ogreResourceManager = Ogre::ResourceGroupManager::getSingleton();
 #ifdef NDEBUG
+  auto &ogreResourceManager = Ogre::ResourceGroupManager::getSingleton();
+
 #if __has_include("programs.zip.h")
   Ogre::EmbeddedZipArchiveFactory::addEmbbeddedFile("programs.zip", programs_zip, sizeof(programs_zip), nullptr);
   ogreResourceManager.addResourceLocation("programs.zip", "EmbeddedZip", Ogre::RGN_INTERNAL);
 #endif
+
+//#if __has_include("assets.zip.h")
+//  Ogre::EmbeddedZipArchiveFactory::addEmbbeddedFile("assets.zip", assets_zip, sizeof(assets_zip), nullptr);
+//  ogreResourceManager.addResourceLocation("assets.zip", "EmbeddedZip", Ogre::RGN_INTERNAL);
+//#endif
+
 #endif
 }
 }  // namespace Glue
