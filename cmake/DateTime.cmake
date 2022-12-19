02db231107fb9b559718b3fe92ea725a1715f8ed
Andrew Vasiliev <foursilico@gmail.com>
2022-12-19 02:41:16 +0200

    cpack target name contains git hash and date

--- cmake/DateTime.cmake
+++ cmake/DateTime.cmake
@@ -4,4 +4,7 @@ if (_datetimes_included)
 endif (_datetimes_included)
 set(_datetimes_included true)
 
-string(TIMESTAMP DATE_TIME "%Y-%m-%dT%H:%M:%S" UTC)
+
+string(TIMESTAMP DATE "%Y-%m-%d" UTC)
+string(TIMESTAMP TIME "%H:%M:%S" UTC)
+string(TIMESTAMP DATE_TIME "%Y-%m-%d:%H:%M:%S" UTC)
