f7e56b2372514a8a306efe23800732b53c571910
Andrew Vasiliev <foursilico@gmail.com>
2022-12-19 05:09:39 +0200

    remove colo symbol(:) from package name

--- cmake/DateTime.cmake
+++ cmake/DateTime.cmake
@@ -5,6 +5,14 @@ endif (_datetimes_included)
 set(_datetimes_included true)
 
 
-string(TIMESTAMP DATE "%Y-%m-%d" UTC)
-string(TIMESTAMP TIME "%H:%M:%S" UTC)
-string(TIMESTAMP DATE_TIME "%Y-%m-%d:%H:%M:%S" UTC)
+string(TIMESTAMP DATE_ISO "%Y-%m-%d" UTC)
+string(TIMESTAMP TIME_ISO "%H:%M:%SZ" UTC)
+string(TIMESTAMP DATE_TIME_ISO "%Y-%m-%dT%H:%M:%SZ" UTC)
+
+string(TIMESTAMP DATE_ISO2 "%Y%m%d" UTC)
+string(TIMESTAMP TIME_ISO2 "%H%M%SZ" UTC)
+string(TIMESTAMP DATE_TIME_ISO2 "%Y%m%dT%H%M%SZ" UTC)
+
+string(TIMESTAMP DATE_CPACK "%Y-%m-%d" UTC)
+string(TIMESTAMP TIME_CPACK "%H-%M-%S" UTC)
+string(TIMESTAMP DATE_TIME_CPACK "%Y-%m-%d_%H-%M-%S" UTC)
