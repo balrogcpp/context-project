15f3dc59088d2b0d576b53374202f19f7a6c96ae
Andrey <foursilico@gmail.com>
2023-04-08 15:52:18 +0300

    rollback part of the path

--- source/Engine/Code/CompositorManager.cpp
+++ source/Engine/Code/CompositorManager.cpp
@@ -393,7 +393,7 @@ static Ogre::Vector4 GetScreenspaceCoords(Ogre::MovableObject *object, Ogre::Cam
   return point;
 }
 
-static Ogre::Vector4 GetLightScreenspaceCoords(Ogre::Light *light, Ogre::Camera *camera) {
+static Ogre::Vector4 GetLightScreenSpaceCoords(Ogre::Light *light, Ogre::Camera *camera) {
   Ogre::Vector4 point = Ogre::Vector4::ZERO;
 
   if (light->getType() == Ogre::Light::LT_DIRECTIONAL)
@@ -417,30 +417,32 @@ static Ogre::Vector4 GetLightScreenspaceCoords(Ogre::Light *light, Ogre::Camera 
 }
 
 void CompositorManager::notifyMaterialRender(Ogre::uint32 pass_id, Ogre::MaterialPtr &mat) {
-  if (pass_id == 10) {  // SSAO
-    const auto &fp = mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
-    fp->setIgnoreMissingParams(true);
-    fp->setNamedConstant("ProjMatrix", Ogre::Matrix4::CLIPSPACE2DTOIMAGESPACE * ogreCamera->getProjectionMatrix());
-    fp->setNamedConstant("InvProjMatrix", Ogre::Matrix4(Ogre::Matrix4::CLIPSPACE2DTOIMAGESPACE * ogreCamera->getProjectionMatrix()).inverse());
-    fp->setNamedConstant("InvViewMatrix", ogreCamera->getProjectionMatrix().inverse());
-    fp->setIgnoreMissingParams(false);
-
-  } else if (pass_id == 11) {  // SSR
+  if (pass_id == 1) {  // SSAO
     const auto &fp = mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
     fp->setIgnoreMissingParams(true);
     fp->setNamedConstant("ProjMatrix", Ogre::Matrix4::CLIPSPACE2DTOIMAGESPACE * ogreCamera->getProjectionMatrix());
     fp->setNamedConstant("InvProjMatrix", Ogre::Matrix4(Ogre::Matrix4::CLIPSPACE2DTOIMAGESPACE * ogreCamera->getProjectionMatrix()).inverse());
     fp->setNamedConstant("InvViewMatrix", ogreCamera->getViewMatrix().inverse());
+    fp->setNamedConstant("FarClipDistance", ogreCamera->getFarClipDistance());
     fp->setIgnoreMissingParams(false);
 
-  } else if (pass_id == 12) { // Rays
+  } else if (pass_id == 2) {  // SSR
+    const auto &fp = mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
+    fp->setIgnoreMissingParams(true);
+    fp->setNamedConstant("ProjMatrix", Ogre::Matrix4::CLIPSPACE2DTOIMAGESPACE * ogreCamera->getProjectionMatrix());
+    fp->setNamedConstant("InvProjMatrix", Ogre::Matrix4(Ogre::Matrix4::CLIPSPACE2DTOIMAGESPACE * ogreCamera->getProjectionMatrix()).inverse());
+    fp->setNamedConstant("InvViewMatrix", ogreCamera->getViewMatrix().inverse());
+    fp->setNamedConstant("FarClipDistance", ogreCamera->getFarClipDistance());
+    fp->setIgnoreMissingParams(false);
+
+  } else if (pass_id == 3) { // Rays
     const auto &fp = mat->getTechnique(0)->getPass(0)->getFragmentProgramParameters();
     const auto &lightList = ogreSceneManager->_getLightsAffectingFrustum();
 
     fp->setIgnoreMissingParams(true);
     Ogre::Real LightPositionViewSpace[OGRE_MAX_SIMULTANEOUS_LIGHTS * 4];
     for (int i = 0; i < lightList.size(); i++) {
-      Ogre::Vector4 point = GetLightScreenspaceCoords(lightList[i], ogreCamera);
+      Ogre::Vector4 point = GetLightScreenSpaceCoords(lightList[i], ogreCamera);
       LightPositionViewSpace[4 * i] = point.x;
       LightPositionViewSpace[4 * i + 1] = point.y;
       LightPositionViewSpace[4 * i + 2] = point.z;
