c16d0994dbc7a28667106ae6465471258cd39357
Andrey <foursilico@gmail.com>
2023-05-01 12:00:11 +0300

    Tests

--- source/Engine/Code/CompositorManager.cpp
+++ source/Engine/Code/CompositorManager.cpp
@@ -459,7 +459,9 @@ void CompositorManager::notifyMaterialRender(Ogre::uint32 pass_id, Ogre::Materia
 
   } else if (pass_id == 0) {  // mrt render
 
-  } else if (pass_id == 99) { //
+  } else if (pass_id == 100) { //
+
+  } else if (pass_id == 99) {
 
   }
 }
