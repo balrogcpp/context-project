f5a28acb9c5c4184b44c7ad53a5ef2cc4a4b581f
Andrey <foursilico@gmail.com>
2023-05-22 01:45:47 +0300

    glow patch (for amd and mali)

--- source/Engine/Code/CompositorManager.cpp
+++ source/Engine/Code/CompositorManager.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 namespace gge {
 CompositorManager::CompositorManager()
-    : fixedViewportSize(true), forceSizeX(-1), forceSizeY(-1), MRT_COMPOSITOR("MRT"), BLOOM_COMPOSITOR("Glow"), mipChainSize(10), mipMask{4, 7, 9} {}
+    : fixedViewportSize(true), forceSizeX(-1), forceSizeY(-1), MRT_COMPOSITOR("MRT"), BLOOM_COMPOSITOR("Glow"), mipChainSize(9), mipMask{4, 7} {}
 
 CompositorManager::~CompositorManager() {}
 
