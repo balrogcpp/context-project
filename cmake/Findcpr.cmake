3f1a5db9bf4a4b0338a73f89770b9623cabace31
Andrey <foursilico@gmail.com>
2023-06-12 20:58:13 +0300

    link network libraries

---
+++ cmake/Findcpr.cmake
@@ -0,0 +1,24 @@
+# https://gitlab.com/kazv/libkazv/-/blob/servant/cmake/Findcpr.cmake
+
+find_path(cpr_INCLUDE_DIR
+        NAMES cpr.h
+        PATH_SUFFIXES cpr
+        )
+find_library(cpr_LIBRARY
+        NAMES cpr
+        )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cpr
+        FOUND_VAR cpr_FOUND
+        REQUIRED_VARS
+        cpr_LIBRARY
+        cpr_INCLUDE_DIR
+        )
+if(cpr_FOUND AND NOT TARGET cpr::cpr)
+    add_library(cpr::cpr UNKNOWN IMPORTED)
+    set_target_properties(cpr::cpr PROPERTIES
+            IMPORTED_LOCATION "${cpr_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${cpr_INCLUDE_DIR};${cpr_INCLUDE_DIR}/.."
+            )
+endif()
