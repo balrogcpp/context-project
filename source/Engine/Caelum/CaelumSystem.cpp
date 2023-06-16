ea419108c80d2486386a26ca63dedc4809c73435
Andrey <foursilico@gmail.com>
2023-06-16 17:08:56 +0300

    try to disable compilation of some caelum functions

--- source/Engine/Caelum/CaelumSystem.cpp
+++ source/Engine/Caelum/CaelumSystem.cpp
@@ -122,7 +122,7 @@ namespace Caelum
         mObserverLongitude = Ogre::Degree(0);
         mUniversalClock->setJulianDay (Astronomy::J2000);
     }
-
+#if OGRE_COMPILER != OGRE_COMPILER_CLANG
     void CaelumSystem::autoConfigure
     (
         CaelumComponent componentsToCreate/* = CAELUM_COMPONENTS_DEFAULT*/
@@ -242,7 +242,7 @@ namespace Caelum
             mCleanup = true;
         }
     }
-
+#endif
     void CaelumSystem::attachViewportImpl (Ogre::Viewport* vp)
     {
         LogManager::getSingleton().getDefaultLog ()->logMessage (
