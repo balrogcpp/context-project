ae5c7dd93648702805df1350b89d32771bae6697
Andrey Vasiliev <foursilico@gmail.com>
2023-07-02 20:24:30 +0300

    update docker compilation scripts

--- compile.bat
+++ compile.bat
@@ -2,10 +2,10 @@
 setlocal EnableDelayedExpansion
 
 SET _INTERPOLATION_0=
-FOR /f "delims=" %%a in ('git rev-parse --short=8 HEAD') DO (SET "_INTERPOLATION_0=!_INTERPOLATION_0! %%a")
-SET "GIT_SHA1=!_INTERPOLATION_0!"
+FOR /f "delims=" %%a in ('git rev-parse --short=8 HEAD') DO (SET _INTERPOLATION_0=!_INTERPOLATION_0! %%a)
+SET GIT_SHA1=!_INTERPOLATION_0!
 docker build -t cross_test . && docker run -it -d cross_test && SET _INTERPOLATION_1=
-FOR /f "delims=" %%a in ('docker ps -alq') DO (SET "_INTERPOLATION_1=!_INTERPOLATION_1! %%a")
+FOR /f "delims=" %%a in ('docker ps -alq') DO (SET _INTERPOLATION_1=!_INTERPOLATION_1! %%a)
 SET CONTAINER_ID=!_INTERPOLATION_1!
-docker cp !CONTAINER_ID!:\var\build\artifacts ./
+docker cp !CONTAINER_ID!:/var/build/artifacts .
 docker stop !CONTAINER_ID!
--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 GIT_SHA1=$(git rev-parse --short=8 HEAD)
-mkdir -p artifacts &&\
 docker build -t cross_test . &&\
 docker run -it -d cross_test &&\
 CONTAINER_ID=$(docker ps -alq)
