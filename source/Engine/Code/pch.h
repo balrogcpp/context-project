f038d5e2ab7b2f9ed0e0c647666b2a908e4d6393
Andrey Vasiliev <foursilico@gmail.com>
2024-04-28 12:49:32 +0300

    ogre procedural build as dependency

--- source/Engine/Code/pch.h
+++ source/Engine/Code/pch.h
@@ -4,7 +4,5 @@
 #include <BtOgre/BtOgre.h>
 #include <OggSound/OgreOggSound.h>
 #include <Ogre.h>
-#include <Overlay/OgreImGuiOverlay.h>
 #include <PagedGeometry/PagedGeometryAll.h>
-#include <Procedural/Procedural.h>
 #include "Platform.h"
