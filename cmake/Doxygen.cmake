3472e344e1abd25a62720423c0fc0cb6f8a94857
Andrey Vasiliev <foursilico@gmail.com>
2024-08-03 18:59:22 +0300

    update gradle files

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -28,7 +28,7 @@ endif ()
 if (${DOXYGEN_FOUND})
     message(STATUS "Doxygen found. Doxygen generation enabled")
     add_custom_target(DoxygenHtml
-            COMMAND ${DOXYGEN_EXECUTABLE} .codedocs
+            COMMAND ${DOXYGEN_EXECUTABLE}
             COMMAND ${CMAKE_COMMAND} -E chdir ${ARTIFACT_DIR}/doxygen/ ${CMAKE_COMMAND} -E tar cf ${ARTIFACT_DIR}/html.zip --format=zip html
             COMMAND ${CMAKE_COMMAND} -E rm -rf ${ARTIFACT_DIR}/doxygen
             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -43,7 +43,7 @@ endif ()
 if (${LATEX_PDFLATEX_FOUND})
     message(STATUS "Latex PDF found. PDF generation enabled")
     add_custom_target(DoxygenPdf
-            COMMAND ${DOXYGEN_EXECUTABLE} .codedocs
+            COMMAND ${DOXYGEN_EXECUTABLE}
             COMMAND ${CMAKE_COMMAND} -E chdir ${ARTIFACT_DIR}/doxygen/latex ${DOXYGEN_MAKE_COMMAND}
             COMMAND ${CMAKE_COMMAND} -E copy ${ARTIFACT_DIR}/doxygen/latex/refman.pdf ${ARTIFACT_DIR}
             COMMAND ${CMAKE_COMMAND} -E rm -rf ${ARTIFACT_DIR}/doxygen
