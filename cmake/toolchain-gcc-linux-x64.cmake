4f605ded906638c3a168c3b4f94bf1b4729775cb
Andrey Vasiliev <foursilico@gmail.com>
2024-09-14 21:02:31 +0300

    cmake: build external targets during config

--- cmake/toolchain-gcc-linux-x64.cmake
+++ cmake/toolchain-gcc-linux-x64.cmake
@@ -1,3 +1,3 @@
-set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
-set(CMAKE_AR aarch64-linux-gnu-ar)
+set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER x86_64-linux-gnu-g++)
+set(CMAKE_AR x86_64-linux-gnu-ar)
--- contrib/patch/theora.cmake
+++ contrib/patch/theora.cmake
@@ -1,5 +1,5 @@
 project(theora C)
-cmake_minimum_required(VERSION 3.5)
+cmake_minimum_required(VERSION 3.15)
 
 # https://github.com/xiph/theora/issues/10#issuecomment-591894142
 
