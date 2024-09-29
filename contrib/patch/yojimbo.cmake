6c61540b41ff744d8fe78f43043ca0870d5f3d97
Andrey Vasiliev <foursilico@gmail.com>
2024-09-29 03:30:27 +0300

    cmake: yojimbo fix for unix

--- contrib/patch/yojimbo.cmake
+++ contrib/patch/yojimbo.cmake
@@ -6,68 +6,68 @@ cmake_minimum_required(VERSION 3.15)
 project(Yojimbo VERSION 1.2.5)
 # Includedirs  
 include_directories(
-    .
-    include
-    serialize
-    sodium
-    netcode
-    reliable
-    tlsf
+        .
+        include
+        serialize
+        sodium
+        netcode
+        reliable
+        tlsf
 )
 # Definitions
 add_compile_definitions(
-    # Debug
-    $<$<CONFIG:Debug>:YOJIMBO_DEBUG>
-    $<$<CONFIG:Debug>:NETCODE_DEBUG>
-    $<$<CONFIG:Debug>:RELIABLE_DEBUG>
-    # Release
-    $<$<CONFIG:Release>:YOJIMBO_RELEASE>
-    $<$<CONFIG:Release>:NETCODE_RELEASE>
-    $<$<CONFIG:Release>:RELIABLE_RELEASE>
+        # Debug
+        $<$<CONFIG:Debug>:YOJIMBO_DEBUG>
+        $<$<CONFIG:Debug>:NETCODE_DEBUG>
+        $<$<CONFIG:Debug>:RELIABLE_DEBUG>
+        # Release
+        $<$<CONFIG:Release>:YOJIMBO_RELEASE>
+        $<$<CONFIG:Release>:NETCODE_RELEASE>
+        $<$<CONFIG:Release>:RELIABLE_RELEASE>
 )
 
 # SODIUM
 file(GLOB sodium_SRC
-    IF(WIN32)
+        IF(WIN32)
         "sodium/*.c"
         "sodium/*.h"
-    ELSE()
+        ELSE()
         "sodium/*.S"
-    ENDIF()
+        ENDIF()
 )
 file(GLOB sodium_H
-    "sodium/*.h"
+        "sodium/*.h"
 )
 add_library(sodium STATIC ${sodium_SRC})
 # set_target_properties(sodium PROPERTIES PUBLIC_HEADER "${sodium_H}")
 
 # NETCODE
-add_library(netcode STATIC 
-    netcode/netcode.c
-    netcode/netcode.h
+add_library(netcode STATIC
+        netcode/netcode.c
+        netcode/netcode.h
 )
 target_compile_definitions(netcode PRIVATE NETCODE_ENABLE_TESTS=1)
 
 # RELIABLE
-add_library(reliable STATIC 
-    reliable/reliable.c
-    reliable/reliable.h
+add_library(reliable STATIC
+        reliable/reliable.c
+        reliable/reliable.h
 )
-target_compile_definitions(reliable PRIVATE RELIABLE_ENABLE_TESTS=1=1)
+target_compile_definitions(reliable PRIVATE RELIABLE_ENABLE_TESTS=1)
 
 # TLSF
-add_library(tlsf STATIC 
-    tlsf/tlsf.c
-    tlsf/tlsf.h
+add_library(tlsf STATIC
+        tlsf/tlsf.c
+        tlsf/tlsf.h
 )
 
 # YOJIMBO
 file(GLOB yojimbo_SRC
-    "include/*.h"
-    "source/*.cpp"
+        "include/*.h"
+        "source/*.cpp"
 )
 file(GLOB yojimbo_H
-    "include/*.h"
+        "include/*.h"
 )
 add_library(yojimbo STATIC ${yojimbo_SRC})
 target_link_libraries(yojimbo sodium)
@@ -77,74 +77,74 @@ install(TARGETS yojimbo sodium EXPORT yojimboConfig PUBLIC_HEADER DESTINATION ${
 install(EXPORT yojimboConfig DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/cmake/yojimbo)
 
 if (WIN32 OR UNIX)
-# CLIENT
-add_executable(client
-    client.cpp
-    shared.h
-)
-target_link_libraries(client
-    yojimbo
-    sodium
-    tlsf
-    netcode
-    reliable
-)
+    # CLIENT
+    add_executable(client
+            client.cpp
+            shared.h
+    )
+    target_link_libraries(client
+            yojimbo
+            tlsf
+            netcode
+            reliable
+            sodium
+    )
 
-# SERVER
-add_executable(server
-    server.cpp
-    shared.h
-)
-target_link_libraries(server
-    yojimbo
-    sodium
-    tlsf
-    netcode
-    reliable
-)
+    # SERVER
+    add_executable(server
+            server.cpp
+            shared.h
+    )
+    target_link_libraries(server
+            yojimbo
+            tlsf
+            netcode
+            reliable
+            sodium
+    )
 
-# LOOPBACK
-add_executable(loopback
-    loopback.cpp
-    shared.h
-)
-target_link_libraries(loopback
-    yojimbo
-    sodium
-    tlsf
-    netcode
-    reliable
-)
+    # LOOPBACK
+    add_executable(loopback
+            loopback.cpp
+            shared.h
+    )
+    target_link_libraries(loopback
+            yojimbo
+            tlsf
+            netcode
+            reliable
+            sodium
+    )
 
-# SOAK
-add_executable(soak
-    soak.cpp
-    shared.h
-)
-target_link_libraries(soak
-    yojimbo
-    sodium
-    tlsf
-    netcode
-    reliable
-)
+    # SOAK
+    add_executable(soak
+            soak.cpp
+            shared.h
+    )
+    target_link_libraries(soak
+            yojimbo
+            tlsf
+            netcode
+            reliable
+            sodium
+    )
 
-install(TARGETS netcode tlsf reliable client server loopback soak)
+    install(TARGETS netcode tlsf reliable client server loopback soak)
 endif ()
 
 if (YOJIMBO_BUILD_TEST)
-# TEST
-add_executable(test
-    test.cpp
-    shared.h
-)
-target_link_libraries(test
-    yojimbo
-    sodium
-    tlsf
-    netcode
-    reliable
-)
-target_compile_definitions(test PRIVATE SERIALIZE_ENABLE_TESTS=1)
-install(TARGETS test)
+    # TEST
+    add_executable(test
+            test.cpp
+            shared.h
+    )
+    target_link_libraries(test
+            yojimbo
+            tlsf
+            netcode
+            reliable
+            sodium
+    )
+    target_compile_definitions(test PRIVATE SERIALIZE_ENABLE_TESTS=1)
+    install(TARGETS test)
 endif ()
