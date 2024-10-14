585d66a87cedb2956626cb98e2497548854e9f95
Andrey Vasiliev <foursilico@gmail.com>
2024-10-14 19:23:06 +0300

    code refactor wip

--- docker.sh
+++ docker.sh
@@ -4,5 +4,5 @@ GIT_SHA1=$(git rev-parse --short=8 HEAD)
 docker build --build-arg GIT_SHA=$GIT_SHA1 -t cross_test . &&\
 docker run -it -d cross_test &&\
 CONTAINER_ID=$(docker ps -alq)
-docker cp $CONTAINER_ID:/var/build/artifacts ./
+docker cp $CONTAINER_ID:/var/build/Artifacts ./
 docker stop $CONTAINER_ID
