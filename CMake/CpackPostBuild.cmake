eaafbffc9fbca8671e17a603b801abc8aae39b64
Andrey Vasiliev <foursilico@gmail.com>
2024-10-15 02:55:28 +0300

    cmake source refactor

--- CMake/CpackPostBuild.cmake
+++ CMake/CpackPostBuild.cmake
@@ -1,9 +1,1 @@
 # do nothing
-
-#if (EXISTS ${CPACK_OUTPUT_FILE_PATH})
-#    find_program(STRIP_EXE NAMES strip-nondeterminism)
-#    if (STRIP_EXE)
-#        message("Stripping cpack atrifact...")
-#        execute_process(COMMAND ${STRIP_EXE} ${CPACK_OUTPUT_FILE_PATH})
-#    endif ()
-#endif ()
