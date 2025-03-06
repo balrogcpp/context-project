c53874946a2959c3b2dfa2999a378b17b760cac1
Andrey Vasiliev <foursilico@gmail.com>
2025-03-06 12:46:47 +0200

    build msvc relwithdebinfo with console setting

--- Source/Tests/Source/main.cpp
+++ Source/Tests/Source/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace gge;
 
-#if defined(_WIN32)
+#if defined(_WIN32) && !defined(DEBUG)
 #include <windows.h>
 INT WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 #else
